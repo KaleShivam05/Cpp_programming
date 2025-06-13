ef3b11d4723968a52ccc579596aa4929247fc28b
KaleShivam05 <shivamkale0510@gmail.com>
2025-06-14 02:28:24 +0530

    Add files via upload

---
+++ GenericDemo1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+template <class T>
+
+T AdditionS(T No1, T No2) //Generic function
+{
+    T iAns = 0;
+    iAns = No1 + No2;
+    return iAns; 
+}
+
+int main()
+{
+    float iValue1 = 0, iValue2 = 0, iRet = 0;
+    
+    cout<<"Enter frist number :\n";
+    cin>>iValue1;
+
+    cout<<"Enter second number :\n";
+    cin>>iValue2;
+
+    iRet = AdditionS(iValue1,iValue2);
+
+    cout<<"Addition is :"<<iRet<<"\n"; 
+
+    return 0;
+}
