2f46a9b585f0387fcb75ca56d4eca18fb1e01e74
KaleShivam05 <shivamkale0510@gmail.com>
2025-06-15 22:13:25 +0530

    Add files via upload

---
+++ Bitwise_operator1.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+typedef unsigned int UINT;
+
+bool CheckBit(UINT iNo)
+{
+    UINT iMask = 67108864; 
+    UINT iResult = 0;
+
+    iResult = iNo & iMask; //logic
+
+    return (iResult == iMask); 
+    
+}
+
+int main()
+{
+    UINT iValue = 0, iRet = 0;
+    bool bRet = false;
+
+    cout<<"Enter number :\n";
+    cin>>iValue;
+
+    bRet = CheckBit(iValue);//function call
+
+    if(true == bRet)
+    {
+        cout<<"27th bit is ON\n";
+    }
+    else
+    {
+        cout<<"27th bit is OFF\n";
+    }
+    
+    return 0;
+}
---
+++ Bitwise_operator1X.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+typedef unsigned int UINT;
+
+bool CheckBit(UINT iNo, UINT iPos)
+{
+    UINT iMask = 1;
+    UINT iResult = 0;
+
+    if(iPos <= 0 || iPos > 32) // filter
+    {
+        cout<<"Invalid bit position\n";
+        return false;
+    }
+
+    iMask = iMask << (iPos - 1); //mask
+
+    iResult = iNo & iMask; //logic
+
+    return (iResult == iMask); 
+    
+}
+
+int main()
+{
+    UINT iValue = 0, iLocation = 0;
+    bool bRet = false;
+
+    cout<<"Enter number :\n";
+    cin>>iValue;
+
+    cout<<"Enter the position :\n";
+    cin>>iLocation;
+
+    bRet = CheckBit(iValue,iLocation); //function call
+
+    if(true == bRet)
+    {
+        cout<<"bit is ON at location "<<iLocation<<"\n";
+    }
+    else
+    {
+        cout<<"bit is OFF at location "<<iLocation<<"\n";
+    }
+    
+    return 0;
+}
